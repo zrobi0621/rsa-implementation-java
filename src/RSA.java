633b3106180dfd66a8c67481e9311bbc8834e2e0
zrobi0621 <33635244+zrobi0621@users.noreply.github.com>
2019-11-20 18:26:45 +0100

    Done

--- src/RSA.java
+++ src/RSA.java
@@ -1,13 +1,23 @@
+import java.lang.management.BufferPoolMXBean;
 import java.math.BigInteger;
 import java.util.Random;
 import java.util.Scanner;
 
 public class RSA {
 
-    private final static BigInteger one      = new BigInteger("1");
-     BigInteger privateKey;
-     BigInteger publicKey;
-     static BigInteger N;
+    public static final BigInteger ZERO = BigInteger.ZERO;
+    public static final BigInteger ONE = BigInteger.ONE;
+    public static final BigInteger TWO = BigInteger.TWO;
+
+    static BigInteger pubP;
+    static BigInteger pubQ;
+    static BigInteger privateKey;
+    static BigInteger publicKey;
+    static BigInteger pubModulus;
+    static BigInteger pubX;
+    static BigInteger pubY;
+    static BigInteger pubEncrypt;
+    static BigInteger pubDecrypt;
 
     public static void RSAInit(int n)
     {
@@ -16,95 +26,167 @@ public class RSA {
         BigInteger p;
         BigInteger q;
 
-        do {
+       do {
             p = new BigInteger(n/2, r);
-        }while (!millerRabin(p.intValue(),2));
+            pubP = p;
+        }while (!millerRabin(p,2));
+
         do {
             q = new BigInteger(n/2, r);
-        }while (!millerRabin(q.intValue(),2));
-        N = p.multiply(q);
+            pubQ = q;
+        }while (!millerRabin(q,2));
 
-        BigInteger phi = (p.subtract(one)).multiply(q.subtract(one));
+        pubModulus = pubP.multiply(pubQ);
 
+        BigInteger phi = (pubP.subtract(ONE)).multiply(pubQ.subtract(ONE));
+
+        BigInteger e = new BigInteger("65567");
+        publicKey = e;  //PK(N,e)
+
+        extendedEuclid(phi,e);
+
+        BigInteger d;
+        if(pubY.compareTo(new BigInteger("0")) < 0)
+        {
+            d = phi.add(pubY);
+        }
+        else
+        {
+            d = pubY.mod(phi);
+        }
+
+        privateKey = d; //SK(N,d)
     }
 
-    public static void extendedEuclid(long a, long b)
+    public static BigInteger RSAEncrypt(BigInteger message)
     {
-        long x = 0, y = 1, lastX = 1, lastY = 0, temp;
-        while (b != 0)
+        return fastModPow(message,publicKey,pubModulus);
+    }
+
+    public static BigInteger RSADecrypt(BigInteger encryptedMessage)
+    {
+        //return fastModPow(encryptedMessage,privateKey,pubModulus);
+        return CRT(encryptedMessage,pubP,pubQ,privateKey);
+    }
+
+    public static BigInteger CRT(BigInteger c, BigInteger p, BigInteger q, BigInteger d)
+    {
+        BigInteger mp = fastModPow(c,d.mod(p.subtract(ONE)),p);
+        BigInteger mq = fastModPow(c,d.mod(q.subtract(ONE)),q);
+
+        BigInteger M = p.multiply(q);
+        extendedEuclid(q,p);
+
+        BigInteger m = ((mp.multiply(pubX)).multiply(q)).add((mq.multiply(pubY)).multiply(p));
+        m = m.mod(M);
+
+        return m;
+    }
+
+    public static BigInteger Euclid(BigInteger a, BigInteger b)
+    {
+        if (b.compareTo(ZERO) == 0) return a;
+        else return Euclid(b, a.mod(b));
+    }
+
+    public static void extendedEuclid(BigInteger a, BigInteger b)
+    {
+        BigInteger x = ZERO;
+        BigInteger y = ONE;
+        BigInteger lastX = ONE;
+        BigInteger lastY = ZERO;
+        BigInteger temp;
+
+        while (b.compareTo(ZERO) != 0)
         {
-            long q = a / b;
-            long r = a % b;
+            BigInteger q = a.divide(b);
+            BigInteger r = a.mod(b);
 
             a = b;
             b = r;
 
             temp = x;
-            x = lastX - q * x;
+            x = lastX.subtract(q.multiply(x));
             lastX = temp;
 
             temp = y;
-            y = lastY - q * y;
+            y = lastY.subtract(q.multiply(y));
             lastY = temp;
         }
-        System.out.println("x : "+ lastX +" y :"+ lastY);
+        pubX = lastX;
+        pubY = lastY;
     }
-    public static boolean millerRabin(int n, int k)
+
+    public static boolean millerRabin(BigInteger n, int k)
     {
-        if ((n < 2) || (n % 2 == 0))
-        {
-            return (n == 2);
+        BigInteger d = n.subtract(ONE);
+        int s = 0;
+        while (d.mod(TWO).equals(ZERO)) {
+            s++;
+            d = d.divide(TWO);
         }
-
-        int s = n - 1;
-
-        while (s % 2 == 0)
-        {
-            s >>= 1;
-        }
-
-        Random r = new Random();
 
         for (int i = 0; i < k; i++) {
-            int a = r.nextInt(n - 1) + 1;
-            int tempS = s;
-            int mod = 1;//
-
-            for (int j = 0; j < tempS; ++j)
-            {
-                mod = (mod * a) % n;
+            Random r = new Random();
+            BigInteger a = BigInteger.valueOf(r.nextInt());
+            boolean pr = testPrime(n, a, s, d);
+            if (!pr) {
+                return false;
             }
-
-            while (tempS != n - 1 && mod != 1 && mod != n - 1)
-            {
-                mod = (mod * mod) % n;
-                tempS *= 2;
-            }
-
-            if (mod != n - 1 && tempS % 2 == 0) return false;
         }
         return true;
     }
 
-    //(a^b) % m
-    public static int fastPow(int a, int b, int m)
-    {
-        int res = 1;
-        for (int i = 0; i < b; i++)
-        {
-            res = res * a;
-            res = res % m;
+    public static boolean testPrime(BigInteger n, BigInteger a, int s, BigInteger d) {
+        for (int i = 0; i < s; i++) {
+            BigInteger exp = TWO.pow(i);
+            exp = exp.multiply(d);
+            BigInteger res = fastModPow(a,exp,n);
+            if (res.equals(n.subtract(ONE)) || res.equals(ONE)) {
+                return true;
+            }
         }
-        return res % m;
+        return false;
     }
 
+    //(a^b) % m
+    public static BigInteger fastModPow(BigInteger base, BigInteger exponent, final BigInteger modulo) {
+        BigInteger result = BigInteger.ONE;
+
+        while (exponent.compareTo(BigInteger.ZERO) > 0)
+        {
+            if (exponent.testBit(0)){
+                result = (result.multiply(base)).mod(modulo);
+            }
+            exponent = exponent.shiftRight(1);
+            base = (base.multiply(base)).mod(modulo);
+        }
+        return result.mod(modulo);
+    }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Message:");
         String message = sc.nextLine();
         byte[] messageBytes = message.getBytes();
         BigInteger bMessage = new BigInteger(messageBytes);
+
+        //BigInteger bMessage = new BigInteger("1814");
+
+        RSAInit(2048);
+
+        BigInteger encrypted = RSAEncrypt(bMessage);
+        pubEncrypt = encrypted;
+
+        BigInteger decrypted = RSADecrypt(encrypted);
+        pubDecrypt = decrypted;
+
+        System.out.println("Message: " + bMessage);
+        System.out.println("Public: " + publicKey);
+        System.out.println("Private: " + privateKey);
+        System.out.println("Encrypted: " + encrypted);
+        System.out.println("Decrypted: " + decrypted);
     }
 }
